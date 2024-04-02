dceae9af64f272dbd3e38082b45baa921bd63461
Marwanmissor <marawanmissor108@gmail.com>
2024-04-03 00:32:11 +0200

    created the project files

---
+++ MonyAPP/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.boshy.monyapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.boshy.monyapp to javafx.fxml;
+    exports com.boshy.monyapp;
+}
