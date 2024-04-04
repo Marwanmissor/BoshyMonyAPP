447e51366e38e8e05f63c72ffc36adee7af52644
Marwanmissor <marawanmissor108@gmail.com>
2024-04-04 14:10:41 +0200

    now wallpaper changed successfully

--- MonyAPP/src/main/java/com/boshy/monyapp/HelloApplication.java
+++ MonyAPP/src/main/java/com/boshy/monyapp/HelloApplication.java
@@ -12,18 +12,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @FXML
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("تجميع الايراد");
         Image icon = new Image(String.valueOf(getClass().getResource("icon.png")));
-        ImageView wallpaperImage  = new ImageView(new Image(String.valueOf(getClass().getResource("ffflux.svg"))));
-        AnchorPane MAP  = new AnchorPane();
-        wallpaperImage.setFitWidth(MAP.getPrefWidth());
-        wallpaperImage.setFitHeight(MAP.getPrefHeight());
-        MAP.getChildren().add(0, wallpaperImage);
         stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
